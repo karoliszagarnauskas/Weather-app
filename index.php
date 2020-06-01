42e6928e4d6b2da7b0b1dc9d381dcc86028e4cd0
karoliszagarnauskas <35958904+karoliszagarnauskas@users.noreply.github.com>
2020-06-01 16:57:39 +0300

    working db connection

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
+<?php
+  include_once 'includes/database_handler.php';
+  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <link rel="shortcut icon" href="favicon.ico" >
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
